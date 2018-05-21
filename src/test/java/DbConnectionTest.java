176868a12489542bc27a2492a0f95b6b7e21a092
Adewale Obadimu <baodium@gmail.com>
2018-05-21 17:46:16 -0500

    Updated DbConnection and tested it

---
+++ src/test/java/DbConnectionTest.java
@@ -0,0 +1,13 @@
+import authentication.*;
+
+public class DbConnectionTest {
+	public static void main(String[] args) {
+		DbConnection testConnection = new DbConnection();
+		System.out.println(testConnection.getConnection());						// Test JDBC Connection
+		System.out.println(testConnection.md5Funct("wale"));
+		
+		testConnection.addUser("abcabc", "pass", "baodium@gmail.com");
+		//testConnection.removeUser("ax");
+		
+	}
+}
