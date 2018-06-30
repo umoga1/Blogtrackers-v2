75f3c6479baed9679a3db74f8ef95be4eda8ae69
ayodele <ayodele@omnibus.com.ng>
2018-06-30 20:46:43 +0100

    update

--- src/main/webapp/pagedependencies/imageloader.js
+++ src/main/webapp/pagedependencies/imageloader.js
@@ -65,7 +65,7 @@ function getImage(image_id,url){
 						
 						nurl = pre+""+nurl;
 						console.log(nurl);
-						scrapeImage(image_id, nurl)
+						//scrapeImage(image_id, nurl);
 					}
 				}
 			}
@@ -101,13 +101,13 @@ function scrapeImage(image_id, url){
 						if(con.length>1 && content[1]!="https://s0.wp.com/i/blank.jpg"){
 													
 							$("."+image_id).html('<img class="card-img-top pt30 pb30" src="'+content[1]+'"  />');
-							$("#"+image_id).remove();
+							//$("#"+image_id).remove();
 							return false;
 						}
 					}
 				}
 			}
-		
+			$("#"+image_id).remove();
 		}
 	});
 	
