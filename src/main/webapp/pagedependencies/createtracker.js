686a855f7d4a0d5c2b546790b3da10b57e3d5ff9
Adekunle Adigun <adigon2006@gmail.com>
2018-07-30 14:26:28 -0500

    tracker list updated

--- src/main/webapp/pagedependencies/createtracker.js
+++ src/main/webapp/pagedependencies/createtracker.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+	
+	 var newtrackersection = '<div class="card noborder curved-card mb30 pt60 pb60 newtrackersection"><div class="card-body"><div class="cursor-pointer"><h4 class="text-primary text-center"><i class="addnewtracker" data-toggle="tooltip" data-placement="top" title="Add New Tracker"></i></h4></div></div></div>';
+	  
+	 
 	// close tracker creation
 	$('.closetracker, .canceltracker').click(function(){
 		
@@ -42,6 +46,7 @@ $(document).ready(function(){
 		 loadCSS("assets/css/toastr.css");
 		 toastr.error("Enter Tracker Name","Error");
 	  });
+  
    }
 //     making sure tracker description is set
    else if(trackerdescription === "")
@@ -52,20 +57,20 @@ $(document).ready(function(){
 			 toastr.error("Enter Tracker Description","Error");
 		  });
     }
-   else if(allblogs.length == 0)
-	   {
-	   $.getScript("assets/js/toastr.js", function(data, textStatus, jqxhr) {
-			 loadCSS("assets/css/toastr.css");
-			 toastr.error("Add blogs to track","Error");
-		  });
-	   }
+//   else if(allblogs.length == 0)
+//	   {
+//	   $.getScript("assets/js/toastr.js", function(data, textStatus, jqxhr) {
+//			 loadCSS("assets/css/toastr.css");
+//			 toastr.error("Add blogs to track","Error");
+//		  });
+//	   }
    else
 	   {
 	   // hide the tooltip
 	   $(".tooltip").hide();
 	    // variable needed are trackername, trackerdescription and (allblogs) which contains arrays of blogs
 	   
-	   trackernamehtmlupdate = '<div class=""><a href="analytics.html"><h1 class="text-primary text-center pt20 pl5 pr5">'+trackername+'</h1></a></div>';
+	   trackernamehtmlupdate = '<div class=""><a href="analytics.html"><h1 class="text-primary text-center pt20 pl10 pr10 bold-text">'+trackername+'</h1></a></div>';
 	   var today = new Date();
 	   var dd = today.getDate();
 	   var mm = today.getMonth()+1; //January is 0!
@@ -90,12 +95,12 @@ $(document).ready(function(){
 	   timesectionhtmlupdate = '<p class="card-text text-center postdate text-primary">'+currentdate+" , "+currenttime+'</p>';	 
 	   bloglisthtmlupdate = '<div class="text-center"><button class="btn btn-default stylebutton5 text-primary p30 pt5 pb5" style="width:100%;">'+allblogcombined+'</button></div>';
 	   trackerdescriptionhtmlupdate = ' <p class="mt20 text-primary text-center">'+trackerdescription+'</p>';
-	   blogcounthtmlupdate = '<div class="text-center mt20"><button class="btn btn-default stylebutton6 text-primary p30 pt5 pb5 text-left" style="width:100%;"><h1 class="text-success mb0">235</h1><h5 class="text-primary">Blogs</h5></button></div>';
-	   postcounthtmlupdate = '<div class="text-center mt10"><button class="btn btn-default stylebutton6 text-primary p30 pt5 pb5 text-left" style="width:100%;"><h1 class="text-success mb0">4,000,232</h1><h5 class="text-primary">Posts</h5></button></div>';
-	   commentcounthtmlupdate = '<div class="text-center mt10"><button class="btn btn-default stylebutton6 text-primary p30 pt5 pb5 text-left" style="width:100%;"><h1 class="text-success mb0">120</h1><h5 class="text-primary">Comments</h5></button></div>';
-	   buttonhtmlupdate = '<div class="pt30 pb20 text-center"><i class="fas fa-chart-line text-primary icontrackersize cursor-pointer proceedtoanalytics" data-toggle="tooltip" data-placement="top" title="Proceed to Analytics"></i><i class="fas fa-sync text-primary icontrackersize cursor-pointer refreshtracker" data-toggle="tooltip" data-placement="top" data-action="reload" title="Refresh Tracker"></i><i class="fas fa-pencil-alt text-primary icontrackersize cursor-pointer edittracker" data-toggle="tooltip" data-placement="top" title="Edit Tracker"></i></div>';
+	   blogcounthtmlupdate = '<div class="text-center mt20"><button class="btn btn-default stylebutton6 text-primary p30 pt5 pb5 text-left" style="width:100%;"><h1 class="text-success mb0">0</h1><h5 class="text-primary">Blogs</h5></button></div>';
+	   postcounthtmlupdate = '<div class="text-center mt10"><button class="btn btn-default stylebutton6 text-primary p30 pt5 pb5 text-left" style="width:100%;"><h1 class="text-success mb0">0</h1><h5 class="text-primary">Posts</h5></button></div>';
+	   commentcounthtmlupdate = '<div class="text-center mt10"><button class="btn btn-default stylebutton6 text-primary p30 pt5 pb5 text-left" style="width:100%;"><h1 class="text-success mb0">0</h1><h5 class="text-primary">Comments</h5></button></div>';
+	   buttonhtmlupdate = '<div class="pt30 pb20 text-center"><i class="text-primary icontrackersize cursor-pointer proceedtoanalytics" data-toggle="tooltip" data-placement="top" title="Proceed to Analytics"></i><i class=" text-primary icontrackersize cursor-pointer refreshtracker" data-toggle="tooltip" data-placement="top" data-action="reload" title="Refresh Tracker"></i><i class=" text-primary icontrackersize cursor-pointer deletetracker" data-toggle="tooltip" data-placement="top" title="Delete Tracker"></i></div>';
 	   $(this).parent().parent().parent().prepend(trackernamehtmlupdate);
-	   $(this).parent().parent().html(timesectionhtmlupdate + bloglisthtmlupdate + trackerdescriptionhtmlupdate + blogcounthtmlupdate + postcounthtmlupdate + commentcounthtmlupdate + buttonhtmlupdate);
+	   $(this).parent().parent().html(bloglisthtmlupdate + trackerdescriptionhtmlupdate + blogcounthtmlupdate + postcounthtmlupdate + commentcounthtmlupdate + buttonhtmlupdate);
 	   
 	  
 	   // get script to refresh tracker
@@ -103,6 +108,12 @@ $(document).ready(function(){
 		  
 		  });
 	   
+	   
+	   // section that shows add new tracker icon after success success after succes
+	   $('.card-columns').prepend(newtrackersection);
+	    $.getScript("pagedependencies/addnewtracker.js", function(data, textStatus, jqxhr) {
+			
+		  });
 	   // create an ajax to create a tracker
 	   }
 	 
