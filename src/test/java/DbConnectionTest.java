fc8f31c451b3224c2767407c3f5c3ef994e9b6fe
Adewale Obadimu <baodium@gmail.com>
2018-08-22 12:44:08 -0500

    fixed issue with baseurl hashmap

--- src/test/java/DbConnectionTest.java
+++ src/test/java/DbConnectionTest.java
@@ -1,11 +1,16 @@
+import java.util.HashMap;
+
 import authentication.*;
 
 
 public class DbConnectionTest {
 	public static void main(String[] args) {
 		DbConnection testConnection = new DbConnection();
-		System.out.println(testConnection.isUserExists("baodium"));
-		//System.out.println(testConnection.getConnection());						// Test JDBC Connection
+		HashMap<String, String> hm = new HashMap<String, String>();
+		
+		hm = DbConnection.loadConstant();		
+		//System.out.println(testConnection.isUserExists("baodium"));
+		System.out.println(hm.get("elasticIndex")+"blogposts/");						// Test JDBC Connection
 		//System.out.println(testConnection.md5Funct("wale"));
 		//testConnection.addUser("abcabc", "pass", "baodium@gmail.com");
 		//testConnection.removeUser("ax");
