5b52e83f3c4f5da19ca087e49f373392a15392ce
Adewale Obadimu <baodium@gmail.com>
2018-10-10 15:07:34 -0500

    update

--- src/main/webapp/pagedependencies/baseurl.js
+++ src/main/webapp/pagedependencies/baseurl.js
@@ -1,4 +1,4 @@
-// var app_url = "http://localhost:8080/Blogtrackers/";
- var app_url = "http://blogtrackers.host.ualr.edu/Blogtrackers/";
+ var app_url = "http://localhost:8080/Blogtrackers/";
+// var app_url = "http://blogtrackers.host.ualr.edu/Blogtrackers/";
   
   var baseurl =  app_url;
