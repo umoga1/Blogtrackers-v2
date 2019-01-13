6863195fd1a59e5512affff6114690c344e15acd
ayodele <ayodele@omnibus.com.ng>
2019-01-13 16:30:24 +0100

    update

--- src/main/webapp/pagedependencies/postingfrequency.js
+++ src/main/webapp/pagedependencies/postingfrequency.js
@@ -143,7 +143,7 @@ function loadInfluence(start_date,end_date){
 			 $.getScript("assets/vendors/DataTables/Buttons-1.5.1/js/buttons.html5.min.js", function(data, textStatus, jqxhr) {	});
 			 $.getScript("assets/vendors/DataTables/Buttons-1.5.1/js/buttons.print.min.js", function(data, textStatus, jqxhr) {	});
 			 
-			 $.getScript("pagedependencies/postingfrequency.js?v=1980", function(data, textStatus, jqxhr) {	});
+			// $.getScript("pagedependencies/postingfrequency.js?v=1980", function(data, textStatus, jqxhr) {	});
 			 
 			 
 			//$("#influence_table").html(response);
